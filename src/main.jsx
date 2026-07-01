7fd8688e054475f0106334efcfed2699e6c10f71
Pirithika <mpirithika13@gmail.com>
2026-07-01 15:33:04 +0530

    Updated project

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 )
